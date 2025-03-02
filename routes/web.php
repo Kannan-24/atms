b081284bec92a1a4dc974b1ed3b0bab8e8b94f32
Kannan-24 <kannanmuruganandham1@gmail.com>
2025-03-02 12:57:26 +0530

    Refactor route definitions for clarity and organization; restore resource routes for departments, drivers, faculty, and students

--- routes/web.php
+++ routes/web.php
@@ -43,49 +43,49 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/departments/import', [DepartmentController::class, 'importForm'])->name('departments.import.form');
     Route::post('/departments/import', [DepartmentController::class, 'import'])->name('departments.import');
-    Route::resource('departments', DepartmentController::class);
-
+    
     //driver routes
-    Route::resource('drivers', DriverController::class);
     Route::get('/buses/{bus}/assign-driver', [BusController::class, 'assignDriverForm'])->name('buses.assigndriverform');
     Route::post('/buses/{bus}/assign-driver', [BusController::class, 'assignDriver'])->name('buses.assignDriver');
     Route::patch('/buses/update-driver-validity/{busDriver}', [BusController::class, 'updateDriverValidity'])->name('buses.updateDriverValidity');
     Route::delete('/buses/remove-driver/{busDriver}', [BusController::class, 'removeDriver'])->name('buses.removeDriver');
     Route::get('/drivers/assign/{driver}', [DriverController::class, 'assign'])->name('drivers.assingdriver');
-
+    
     // Faculty Routes
-    Route::resource('faculty', FacultyController::class);
     Route::get('/buses/{bus}/assign-faculty', [BusController::class, 'assignFacultyForm'])->name('buses.assignfacultyform');
     Route::post('/buses/{bus}/assign-faculty', [BusController::class, 'assignFaculty'])->name('buses.assignFaculty');
     Route::get('/faculties/assign/{faculty}', [FacultyController::class, 'facultyAssign'])->name('faculty.assignFaculty');
     Route::delete('/buses/{facultyIncharge}/remove', [BusController::class, 'removeFacultyIncharge'])->name('buses.removeFacultyIncharge');
-
+    
     // student routes
     Route::get('/students/import', [StudentController::class, 'showImportForm'])->name('students.import.form');
     Route::post('/students/import', [StudentController::class, 'import'])->name('students.import');
-    Route::resource('students', StudentController::class);
     Route::get('/students/{student}/assign-stops', [StudentController::class, 'assignStops'])->name('students.assignStops');
     Route::put('/students/{student}/assign-stops', [StudentController::class, 'updateAssignedStop'])->name('students.assignStops.update');
     Route::get('/students/{student}/edit-stop', [StudentController::class, 'editStop'])->name('students.editStop');
     Route::post('/students/{student}/update-stop', [StudentController::class, 'updateAssignedStop'])->name('students.updateStop');
-
+    
     Route::get('/locations/{busId}', [BusController::class, 'locations'])->name('locations');
     Route::get('/track-buses', [BusController::class, 'trackBuses'])->name('track-buses.index');
     Route::get('/track-bus/{bus}', [BusController::class, 'track'])->name('trackBus.track');
-
-
+    
+    
     // Route Stops
     Route::get('/busroutes/{route}/assignStops', [RouteController::class, 'assignStops'])->name('busroutes.assignStops');
     Route::post('/busroutes/{route}/storeAssignedStops', [RouteController::class, 'storeAssignedStops'])->name('busroutes.storeAssignedStops');
-
+    
     // Batch Routes
     Route::get('/batches/import', [BatchesController::class, 'importForm'])->name('batches.import.form');
     Route::post('/batches/import', [BatchesController::class, 'import'])->name('batches.import');
-
+    
     Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
     Route::get('/attendance/{bus_id}', [AttendanceController::class, 'show'])->name('attendance.show');
-
-
+    
+    
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('drivers', DriverController::class);
+    Route::resource('faculty', FacultyController::class);
+    Route::resource('students', StudentController::class);
     Route::resource('batches', BatchesController::class);
     Route::resource('classes', ClassesController::class);
     Route::resource('parents', ParentsController::class);
